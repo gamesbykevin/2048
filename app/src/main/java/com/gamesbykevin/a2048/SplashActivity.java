f247da1422db36f13d373848a68f741a859ef491
gamesbykevin <kevin@gamesbykevin.com>
2017-05-16 22:52:33 -0500

    Adding splash screen, still needs work

---
+++ app/src/main/java/com/gamesbykevin/a2048/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.gamesbykevin.a2048;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+
+            }
+
+        }, 3000);
+    }
+
+}
